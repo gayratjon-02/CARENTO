9fc59c48b8d365229972a0eabdcd610b0021bf1c
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-11-17 02:50:51 +0900

    feat: configure env  variables

--- apps/carento-batch/src/carento-batch.module.ts
+++ apps/carento-batch/src/carento-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarentoBatchController } from './carento-batch.controller';
 import { CarentoBatchService } from './carento-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [CarentoBatchController],
   providers: [CarentoBatchService],
 })
