fe66eb232509d52666a5f0d3d69e61da31623d19
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-11-19 02:17:20 +0900

    feat: create components related modules

--- apps/carento-api/src/components/components.module.ts
+++ apps/carento-api/src/components/components.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { CarsModule } from './cars/cars.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { ArticleModule } from './article/article.module';
+import { BookingModule } from './booking/booking.module';
+import { NoticeModule } from './notice/notice.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [MemberModule, CarsModule]
+	imports: [MemberModule, CarsModule, AuthModule, CommentModule, LikeModule, ViewModule, FollowModule, ArticleModule, BookingModule, NoticeModule, NotificationModule],
 })
 export class ComponentsModule {}
