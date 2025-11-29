d81188b40a3976af466e27afbf091bbb5eaf2e51
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-11-29 18:46:50 +0900

    feat: create getComments GraphQL Api

--- apps/carento-api/src/components/comment/comment.resolver.ts
+++ apps/carento-api/src/components/comment/comment.resolver.ts
@@ -1,13 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Comment } from '../../libs/dto/comment/comment';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { ObjectId } from 'mongoose';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -33,5 +34,16 @@ export class CommentResolver {
 		console.log('Mutation: updateComment');
 		input._id = shapeIntoMongoObjectId(input._id);
 		return this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
 	}
 }
--- apps/carento-api/src/components/comment/comment.service.ts
+++ apps/carento-api/src/components/comment/comment.service.ts
@@ -3,12 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { ArticleService } from '../article/article.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
-import { Message } from '../../libs/enums/common.enum';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import { Direction, Message } from '../../libs/enums/common.enum';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { CarsService } from '../cars/cars.service';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { T } from '../../libs/types/common';
+import { lookupMember } from '../../libs/config';
 
 @Injectable()
 export class CommentService {
@@ -72,5 +74,35 @@ export class CommentService {
 
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 		return result;
+	}
+
+	// get comments
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		const result: Comments[] = await this.commentModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							//meLiked
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
 	}
 }
