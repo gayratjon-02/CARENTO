5faa1a6ddc2bfaf20bf821a51c56baa764cd7542
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-11-29 19:28:20 +0900

    feat: create Database module on Carento-batch server

--- apps/carento-batch/src/carento-batch.module.ts
+++ apps/carento-batch/src/carento-batch.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarentoBatchController } from './carento-batch.controller';
 import { CarentoBatchService } from './carento-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [CarentoBatchController],
   providers: [CarentoBatchService],
 })
