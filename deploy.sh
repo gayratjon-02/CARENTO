37b918ce3b91be3c9c2d0ed3ce0223a2d2bf7526
gayratjon-02 <s-202386005@sillain.ac.kr>
2026-02-06 00:42:24 +0900

    SERVER

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,10 @@ git reset --hard
 git checkout master
 git pull origin master
 
+docker stop  carento-api
+docker remofe  carento-api
+docker stop  carento-batch
+docker remove carento-batch
+
 
 docker compose up -d
